1e0a78aed2456d7e2de5c98d8885adfb12b4047b
VladosK0k0s <p.s.vlad2014@gmail.com>
2020-10-03 02:10:44 +0300

    added sorry

---
+++ pages/sorrypage.js
@@ -0,0 +1,13 @@
+import Sorry from "../components/SorryPage/SorryPage";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+export default function SorryPage() {
+    return (
+        <div className="App">
+            <Header />
+            <Sorry />
+            <Footer />
+        </div>
+    );
+}
